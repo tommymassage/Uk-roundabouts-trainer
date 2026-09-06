1f0c8eea6429d0e54a5feeb379e05f72892862a1
tommymassage <tommy.massage@yahoo.co.uk>
2026-09-06 16:09:31 +0100

    Use approved user-traced routes for all four exits

--- app/src/main/java/uk/co/roundaboutstrainer/UserTracedRouteLibrary.java
+++ app/src/main/java/uk/co/roundaboutstrainer/UserTracedRouteLibrary.java
@@ -1,49 +1,44 @@
 package uk.co.roundaboutstrainer;
 
 /**
- * Normalised route centre-lines traced by the user on the approved 2-lane roundabout view.
- * Coordinates are 0..1 across the full simulator canvas.
- * These are the source-of-truth routes for the 6 o'clock approach.
- *
- * Current unmarked-roundabout rule used by the simulator:
- * - exits at 12 o'clock or before: left approach lane + outer arc
- * - exits after 12 o'clock: right approach lane + inner arc, then move out for the exit
+ * Normalised centre-lines extracted from the user's approved red traces.
+ * Source of truth: user's 6 o'clock approach drawings.
+ * Unmarked-roundabout rule: exits at/before 12 use left approach + outer arc;
+ * exits after 12 use right approach + inner arc, moving outward before exit.
  */
 public final class UserTracedRouteLibrary {
     private UserTracedRouteLibrary() {}
 
-    // 1st exit: 6 -> 9 o'clock. LEFT approach lane, OUTER arc.
     private static final float[][] FIRST_EXIT = {
-            {0.339f,0.870f},{0.350f,0.809f},{0.350f,0.748f},{0.342f,0.688f},
-            {0.335f,0.627f},{0.318f,0.566f},{0.284f,0.504f},{0.255f,0.464f},
-            {0.209f,0.424f},{0.163f,0.395f},{0.117f,0.376f},{0.072f,0.358f},{0.029f,0.346f}
+        {0.3366f,0.8932f},{0.3405f,0.8498f},{0.3503f,0.8064f},{0.3509f,0.7630f},
+        {0.3457f,0.7196f},{0.3411f,0.6753f},{0.3359f,0.6319f},{0.3268f,0.5885f},
+        {0.3086f,0.5451f},{0.2819f,0.5009f},{0.2507f,0.4592f},{0.2181f,0.4297f},
+        {0.1855f,0.4089f},{0.1523f,0.3889f},{0.1198f,0.3759f},{0.0872f,0.3637f},
+        {0.0547f,0.3533f},{0.0215f,0.3438f}
     };
 
-    // 2nd exit: 6 -> 12 o'clock. LEFT approach lane, OUTER arc.
     private static final float[][] SECOND_EXIT = {
-            {0.343f,0.867f},{0.352f,0.809f},{0.358f,0.749f},{0.356f,0.687f},
-            {0.340f,0.627f},{0.315f,0.567f},{0.283f,0.504f},{0.252f,0.444f},
-            {0.232f,0.384f},{0.219f,0.323f},{0.235f,0.261f},{0.314f,0.194f},
-            {0.413f,0.142f},{0.453f,0.081f},{0.471f,0.020f}
+        {0.3411f,0.8785f},{0.3503f,0.8203f},{0.3581f,0.7613f},{0.3574f,0.7031f},
+        {0.3438f,0.6441f},{0.3255f,0.5859f},{0.2956f,0.5269f},{0.2624f,0.4679f},
+        {0.2389f,0.4097f},{0.2227f,0.3507f},{0.2233f,0.2925f},{0.2513f,0.2370f},
+        {0.2956f,0.2049f},{0.3392f,0.1884f},{0.3835f,0.1727f},{0.4258f,0.1250f},
+        {0.4590f,0.0660f},{0.4772f,0.0069f}
     };
 
-    // 3rd exit: 6 -> 3 o'clock. RIGHT approach lane, INNER arc, then move out to exit.
-    // Derived from the user's red trace and smoothed by the renderer.
     private static final float[][] THIRD_EXIT = {
-            {0.442f,0.870f},{0.445f,0.790f},{0.444f,0.705f},{0.433f,0.615f},
-            {0.393f,0.525f},{0.340f,0.440f},{0.317f,0.350f},{0.331f,0.270f},
-            {0.390f,0.215f},{0.500f,0.180f},{0.610f,0.180f},{0.700f,0.200f},
-            {0.770f,0.220f},{0.850f,0.235f},{0.920f,0.240f},{0.985f,0.245f}
+        {0.4427f,0.8620f},{0.4421f,0.7821f},{0.4453f,0.7014f},{0.4362f,0.6215f},
+        {0.4069f,0.5408f},{0.3555f,0.4609f},{0.3268f,0.3811f},{0.3151f,0.3003f},
+        {0.3561f,0.2240f},{0.4160f,0.1840f},{0.4766f,0.1675f},{0.5371f,0.1589f},
+        {0.5970f,0.1727f},{0.6576f,0.2005f},{0.7181f,0.2214f},{0.7780f,0.2283f},
+        {0.8385f,0.2309f},{0.8991f,0.2378f}
     };
 
-    // 4th exit / U-turn: 6 -> 6 o'clock. RIGHT approach lane, INNER arc,
-    // then move out and leave on the southbound exit.
     private static final float[][] FOURTH_EXIT = {
-            {0.462f,0.875f},{0.460f,0.790f},{0.455f,0.700f},{0.435f,0.610f},
-            {0.395f,0.520f},{0.340f,0.440f},{0.310f,0.350f},{0.320f,0.270f},
-            {0.390f,0.215f},{0.500f,0.180f},{0.610f,0.190f},{0.670f,0.230f},
-            {0.690f,0.320f},{0.680f,0.420f},{0.650f,0.520f},{0.620f,0.610f},
-            {0.595f,0.700f},{0.585f,0.790f},{0.590f,0.875f}
+        {0.4609f,0.8715f},{0.4701f,0.7708f},{0.4557f,0.6693f},{0.4251f,0.5677f},
+        {0.3841f,0.4670f},{0.3249f,0.3689f},{0.3184f,0.2674f},{0.3867f,0.2075f},
+        {0.4629f,0.1840f},{0.5391f,0.1840f},{0.6152f,0.2057f},{0.6790f,0.2908f},
+        {0.6842f,0.3924f},{0.6628f,0.4939f},{0.6445f,0.5946f},{0.6191f,0.6962f},
+        {0.6191f,0.7977f},{0.6393f,0.7969f}
     };
 
     public static float[][] pointsForExit(int exit) {
@@ -53,25 +48,12 @@ public final class UserTracedRouteLibrary {
         if (exit == 4) return copy(FOURTH_EXIT);
         return new float[0][0];
     }
-
-    public static boolean hasUserTrace(int exit) {
-        return exit >= 1 && exit <= 4;
-    }
-
-    public static boolean usesLeftApproachLane(int exit) {
-        return exit <= 2;
-    }
-
-    public static boolean usesOuterArc(int exit) {
-        return exit <= 2;
-    }
-
+    public static boolean hasUserTrace(int exit) { return exit >= 1 && exit <= 4; }
+    public static boolean usesLeftApproachLane(int exit) { return exit <= 2; }
+    public static boolean usesOuterArc(int exit) { return exit <= 2; }
     private static float[][] copy(float[][] src) {
-        float[][] out = new float[src.length][2];
-        for (int i=0;i<src.length;i++) {
-            out[i][0] = src[i][0];
-            out[i][1] = src[i][1];
-        }
+        float[][] out=new float[src.length][2];
+        for(int i=0;i<src.length;i++){ out[i][0]=src[i][0]; out[i][1]=src[i][1]; }
         return out;
     }
 }
